acaa668d4c312117cf3d520f0c93b1450be7ede1
Mohd Salimullah <ajadi273301@gmail.com>
2022-11-27 18:22:32 +0530

    salim khan first commit

---
+++ src/main/java/com/corespring/Address.java
@@ -0,0 +1,86 @@
+package com.corespring;
+
+public class Address {
+
+	private int id;
+	private String address;
+	
+	
+	
+	
+
+	
+	public Address() {
+		super();
+	}
+
+
+
+
+
+
+	public Address(int id, String address) {
+		super();
+		this.id = id;
+		this.address = address;
+	}
+
+
+
+
+
+
+	public void display() {
+		System.out.println(address);
+	}
+
+
+
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+
+
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+
+
+
+
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", address=" + address + "]";
+	}
+	
+	
+	
+
+}
